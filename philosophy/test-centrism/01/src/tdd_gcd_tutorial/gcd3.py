22784029cc11febb0c1ca4993a2dfd2379bea1a7
YoheiWatanabe <ywatanabevlt@icloud.com>
2026-03-19 03:53:54 +0900

    Paper 01 was revised up to Axiom 8.

--- philosophy/test-centrism/01/src/tdd_gcd_tutorial/gcd3.py
+++ philosophy/test-centrism/01/src/tdd_gcd_tutorial/gcd3.py
@@ -1,3 +1,10 @@
 def gcd3(a: int, b: int) -> int:
     # Return the smaller number of a and b.
     return min(a, b)
+
+# def gcd3(a: int, b: int) -> int:
+#     # Return the smaller number of a and b.
+#     c = b
+#     if a < b:
+#         c = a
+#     return c
--- philosophy/test-centrism/01/src/tdd_gcd_tutorial/gcd4.py
+++ philosophy/test-centrism/01/src/tdd_gcd_tutorial/gcd4.py
@@ -1,8 +1,15 @@
 def gcd4(a: int, b: int) -> int:
-    # Divide the larger number by the smaller one. If there is a remainder, return the remainder; if it divides evenly, return the smaller number.
-    x = max(a, b)
-    y = min(a, b)
-    r = x % y
-    if r:
-        y = r
-    return y
+    # One step of the Euclidean algorithm.
+    # Replace the smaller number with the remainder.
+    a, b = max(a, b), min(a, b)
+    return a % b if a % b else b
+
+# def gcd4(a: int, b: int) -> int:
+    # One step of the Euclidean algorithm.
+    # Replace the smaller number with the remainder.
+#     x = max(a, b)
+#     y = min(a, b)
+#     r = x % y
+#     if r:
+#         y = r
+#     return y
